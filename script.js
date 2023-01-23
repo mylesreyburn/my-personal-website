89088b20a9ae4a6b9e45a5b83e0837cd7be26d4a
Myles Reyburn <mylesreyburn@gmail.com>
2023-01-23 03:35:59 -0700

    added body text, added button and nonfunctional script for the button to work

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
-const h2 = document.createElement("h2");
-h2.textContent = "This content added by Javascript";
+function addingEventListener() {
+    const input = document.getElementById('button');
+}
 
-document.querySelector("body").appendChild(h2);
+function clickAlert() {
+    alert('I was clicked!');
+}
+
+input.addEventListener('click', clickAlert());
